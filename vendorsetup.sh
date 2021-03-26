6ad3df4fbd6b03bb04eb69e70f90b7f5543d558c
Julien Milletre Akipe <code.julien@milletre.fr>
2021-03-26 13:43:32 +0000

    FP3: Add vendorsetup.sh to be be able to manually build the recovery
    Change-Id: Ib04d01396dfd8af46402ae0cb164b9b22e971cbf

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Avoid build errors
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
+
+add_lunch_combo omni_FP3-userdebug  # For users
+add_lunch_combo omni_FP3-eng  # To debug during development
